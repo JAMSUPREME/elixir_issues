fe6c3724a5dac79d03b023263a98db7b2a68c8cb
JUSTIN SPENCER <justin.spencer@excella.com>
2017-03-17 14:31:29 -0400

    Boilerplate

---
+++ lib/issues_app.ex
@@ -0,0 +1,19 @@
+defmodule IssuesApp do
+  @moduledoc """
+  Gets github issues.
+  Super cool!
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> IssuesApp.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/issues_app_test.exs
@@ -0,0 +1,8 @@
+defmodule IssuesAppTest do
+  use ExUnit.Case
+  doctest IssuesApp
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
